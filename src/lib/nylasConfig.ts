488dbfae09f7e3c8907ed39c3c5206ba1913bfec
Hasib <hasibshaif1@gmail.com>
2024-11-17 01:49:09 -0500

    .

--- src/lib/nylasConfig.ts
+++ src/lib/nylasConfig.ts
@@ -1,13 +1,13 @@
 import Nylas from "nylas";
 
 export const nylasConfig = {
-    clientId: process.env.NYLAS_CLIENT_ID!,
-    callbackUri: "https://kronicle.vercel.app/api/oauth/exchange",
-    apiKey: process.env.NYLAS_API_KEY!,
-    apiUri: process.env.NYLAS_API_URI!,
+    clientId: process.env.NYLAS_CLIENT_ID,
+    callbackUri: process.env.NEXT_PUBLIC_URL + "/api/oauth/exchange",
+    apiKey: process.env.NYLAS_API_KEY,
+    apiUri: process.env.NYLAS_API_URI,
 };
 
 export const nylas = new Nylas({
-    apiKey: nylasConfig.apiKey,
+    apiKey: nylasConfig.apiKey as string,
     apiUri: nylasConfig.apiUri,
 });
