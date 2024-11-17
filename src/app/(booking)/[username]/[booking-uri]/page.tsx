cfef0f9c17ecaf9c99e54ebd8b1d7825e278e8cb
Hasib <hasibshaif1@gmail.com>
2024-11-17 01:26:11 -0500

    .

--- src/app/(booking)/[username]/[booking-uri]/page.tsx
+++ src/app/(booking)/[username]/[booking-uri]/page.tsx
@@ -4,35 +4,38 @@ import { ProfileModel } from "@/models/Profile";
 import mongoose from "mongoose";
 
 type PageProps = {
-    params:{
-        username:string;
-        "booking-uri":string;
-    },
-}
+    params: Promise<{
+      username: string;
+      "booking-uri": string;
+    }>;
+  };
+  
 
-export default async function BookingPage(props: PageProps) {
+  export default async function BookingPage(props: PageProps) {
+    const resolvedParams = await props.params;
+  
     await mongoose.connect(process.env.MONGODB_URI as string);
     const profileDoc = await ProfileModel.findOne({
-        username: props.params.username,
-    })
+      username: resolvedParams.username,
+    });
     if (!profileDoc) {
-        return '404';
+      return '404';
     }
     const etDoc = await EventTypeModel.findOne({
-        email: profileDoc.email,
-        uri: props.params?.['booking-uri'],
+      email: profileDoc.email,
+      uri: resolvedParams['booking-uri'],
     });
     if (!etDoc) {
-        return '404';
+      return '404';
     }
     return (
       <div className="bg-gray-800 grow">
         <TimePicker 
-          username={props.params.username}
+          username={resolvedParams.username}
           meetingUri={etDoc.uri}
           length={etDoc.length} 
           bookingTimes={JSON.parse(JSON.stringify(etDoc.bookingTimes))} 
         />
       </div>
     );      
-}
+  }
