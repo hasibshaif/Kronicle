ab3d94c77bc41fb6a9509b847d3269f3b2742350
Hasib <hasibshaif1@gmail.com>
2024-11-17 01:36:40 -0500

    .

--- src/app/api/oauth/exchange/route.ts
+++ src/app/api/oauth/exchange/route.ts
@@ -1,46 +1,46 @@
-import { NextApiRequest } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import { nylasConfig, nylas } from "@/lib/nylasConfig";
 import { session } from "@/lib/session";
-import { redirect } from "next/navigation";
 import { ProfileModel } from "@/models/Profile";
 import mongoose from "mongoose";
 
-export async function GET(req: NextApiRequest) {
-    console.log("Received callback from Nylas");
-    
-    if (!req.url) {
-        return Response.json("Invalid request URL", {status: 400});
-    }
-    
-    const url = new URL(req.url);
-    const code = url.searchParams.get('code');
-  
-    if (!code) {
-        return Response.json("No authorization code returned from Nylas", {status: 400});
-    }
+export async function GET(req: NextRequest) {
+  console.log("Received callback from Nylas");
 
-    const codeExchangePayload = {
-        clientSecret: nylasConfig.apiKey,
-        clientId: nylasConfig.clientId as string,
-        redirectUri: nylasConfig.callbackUri,
-        code,
-    };
-  
-    const response = await nylas.auth.exchangeCodeForToken(codeExchangePayload)
+  const url = new URL(req.url);
+  const code = url.searchParams.get("code");
+
+  if (!code) {
+    return NextResponse.json("No authorization code returned from Nylas", { status: 400 });
+  }
+
+  const codeExchangePayload = {
+    clientSecret: nylasConfig.apiKey,
+    clientId: nylasConfig.clientId as string,
+    redirectUri: nylasConfig.callbackUri,
+    code,
+  };
+
+  try {
+    const response = await nylas.auth.exchangeCodeForToken(codeExchangePayload);
     const { grantId, email } = response;
 
     await mongoose.connect(process.env.MONGODB_URI as string);
 
-    const profileDoc = await ProfileModel.findOne({email});
+    const profileDoc = await ProfileModel.findOne({ email });
     if (profileDoc) {
-        profileDoc.grantId = grantId;
-        await profileDoc.save();
+      profileDoc.grantId = grantId;
+      await profileDoc.save();
     } else {
-        await ProfileModel.create({email, grantId});
+      await ProfileModel.create({ email, grantId });
     }
 
-    await session().set('grantId', grantId);
-    await session().set('email', email);
+    await session().set("grantId", grantId);
+    await session().set("email", email);
 
-    redirect('/');
+    return NextResponse.redirect("/");
+  } catch (error) {
+    console.error("Error during Nylas OAuth exchange:", error);
+    return NextResponse.json("Internal Server Error", { status: 500 });
+  }
 }
